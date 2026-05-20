3d13a92e835796a96c67ac511c7de7fe92024fdb
lumbasi-wabomba <lumbasiwk@gmail.com>
2026-05-20 11:52:51 +0300

    additional files

--- myapplication/demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.fx.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.fx.demo to javafx.fxml;
-    exports com.fx.demo;
-}
--- rmi_project/Animate.java
+++ rmi_project/Animate.java
@@ -1,7 +1,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Hello extends Remote {
+public interface Animate extends Remote {
     void PrintMsg() throws RemoteException;
 }
 
